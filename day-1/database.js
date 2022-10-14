bc28be92edb6d493fd6e70f8888c8d9fdd00e194
Purple-syrup <godhanded0@gmail.com>
2022-10-14 09:26:06 +0000

    day 1

---
+++ day-1/database.js
@@ -0,0 +1,29 @@
+const var1 = "";
+const var2 = true;
+let var3;
+const var4 = null;
+
+console.log(typeof (var1), typeof (var2), typeof (var3), typeof (var4))
+
+let variable1;
+let variable2;
+let variable3;
+let variable4;
+
+
+let variable5 = 90;
+let variable6 = true;
+let variable7 = 56;
+let variable8 = "foo";
+
+let country = "Egypt";
+let maritalStatus = undefined;
+let lastName = "God" + "handed";
+let firstName = "Godand";
+let age = [27, 20, 19, 25, 22]
+
+let fName = "Godand", lName = "Godard", age2 = BigInt, mStatus = null, contry = "Egypt";
+
+const myAge=age,yourAge=23
+
+console.log(myAge,yourAge)
---
+++ day-1/variable.js
@@ -0,0 +1,4 @@
+const var1= String;
+const var2= Boolean;
+let var3;
+const var4= null;
